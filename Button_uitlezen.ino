e8f25b6ec2c4d742652d175336febca772a894a1
WesselRo <123960059+WesselRo@users.noreply.github.com>
2024-10-24 11:34:48 +0200

    Add files via upload

---
+++ Button_uitlezen.ino
@@ -0,0 +1,77 @@
+// Button uitlezen
+
+const int buttonPin = 18;
+const int ledBusy = 21;
+const int ledDone = 19;
+const int ledWaiting = 4;
+const int trigPin = 22;  
+const int echoPin = 23; 
+
+float duration, distance;  
+
+int buttonState = 0;
+
+bool waiting = false;
+
+int neededDistance = 5;
+
+void setup() {
+  pinMode(ledBusy, OUTPUT);
+  pinMode(ledDone, OUTPUT);
+  pinMode(ledWaiting, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(trigPin, OUTPUT);  
+	pinMode(echoPin, INPUT);  
+	Serial.begin(9600);  
+}
+
+
+void loop() {
+  // bereken afstand met ultrasoon
+  digitalWrite(trigPin, LOW);  
+	delayMicroseconds(2);  
+	digitalWrite(trigPin, HIGH);  
+	delayMicroseconds(10);  
+	digitalWrite(trigPin, LOW);  
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;  
+
+  Serial.print("Distance: ");  
+	Serial.println(distance); 
+
+  buttonState = digitalRead(buttonPin);
+ 
+  // Als de PLC (knop) een signaal stuurt en checkt hij of er een frikandel ligt
+  // Voorraad? roept hij de methode dropSnack() aan
+  // Geen voorraad? wacht hij tot er een frikandel is voor hij dropSnack() aanroept
+  if (buttonState == HIGH){
+    if (distance < neededDistance){
+      dropSnack();
+      waiting = false;
+    } else {
+      waiting = true;
+      digitalWrite(ledWaiting, HIGH);
+    }
+  }
+
+  if (waiting && distance < neededDistance){
+    digitalWrite(ledWaiting, LOW);
+    dropSnack();
+    waiting = false;
+  }
+
+  // kleine delay voor een goede werking van de ultrasoon sensor
+  delay(100);
+}
+
+void dropSnack(){
+  // stappenmotor 180 graden draaien
+  digitalWrite(ledBusy, HIGH);
+  delay (2000);
+  digitalWrite(ledBusy, LOW);
+
+  //doorgeven dat er een snack is gedropt
+  digitalWrite(ledDone, HIGH);
+  delay(500);
+  digitalWrite(ledDone, LOW);
+}
